e56e86c34cf3c3cf63f15890e20a5f7f2507e7c2
hannikagabrielsson <r.hannika@gmail.com>
2024-02-14 19:14:27 +0200

    complete exercise 2.5

---
+++ src/Course.jsx
@@ -0,0 +1,85 @@
+
+const courses = [
+  {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }, 
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+    ]
+  }
+]
+
+const Content = () =>{
+
+  return(
+    <>
+    {
+      courses.map(course =>
+        (
+          <div key={course.id}>
+            <h1>{course.name}</h1>
+            {
+              course.parts.map(part =>
+                (
+                <div key={part.id}>
+                  <p>{part.name} {part.exercises}</p>                               
+                </div>
+              ))
+              }
+              <p><strong>Total of {course.parts.reduce((total, part) => total + part.exercises, 0)} exercises</strong></p>
+          </div>
+
+        ))
+    }
+    </>
+  )
+}
+
+
+const Course = () => {
+  return (
+    <div>
+      <Content />
+    </div>
+  )
+}
+
+export default Course
+
+
+ 
