dec823907a58aa1eeff0f8a6964e9eb59dcdfd0b
hannikagabrielsson <r.hannika@gmail.com>
2024-02-14 19:29:29 +0200

    create github page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-perusteet-Osa1-kurssitiedot",
+  base: "/React-perusteet-Osa2-kurssitiedot",
   plugins: [react()],
 })
